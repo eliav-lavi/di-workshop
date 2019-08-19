839e8731370aa3eaf0c7817baefa2370e3a6a908
Eliav Lavi <eliavlavi@gmail.com>
2019-08-19 22:10:03 +0300

    added inspect route

--- lib/app.rb
+++ lib/app.rb
@@ -14,6 +14,7 @@ class App < Roda
   route do |r|
     r.post 'check_in' do
       begin
+        puts "*****"
         body = JSON.parse(r.body.read).map { |k,v| [k.to_sym, v] }.to_h
         dry = Parka::CheckIn::Request.new(body)
         response = Parka::CheckIn::Service.build.call(dry)
@@ -29,6 +30,7 @@ class App < Roda
 
     r.post 'check_out' do
       begin
+        puts "*****"
         body = JSON.parse(r.body.read).map { |k,v| [k.to_sym, v] }.to_h
         dry = Parka::CheckOut::Request.new(body)
         response = Parka::CheckOut::Service.build.call(dry)
@@ -36,6 +38,19 @@ class App < Roda
         puts "*****"
 
         { data: response }
+      rescue StandardError => e
+        puts "ERROR: #{e.message} | backtrace: #{e.backtrace}"
+        { error: e.message, backtrace: e.backtrace }
+      end
+    end
+
+    r.get 'inspect' do
+      begin
+        puts "*****"
+        puts "db state: #{Parka::Db.instance.inspect}"
+        puts "*****"
+        
+        { data: 'see terminal to inspect db state' }
       rescue StandardError => e
         puts "ERROR: #{e.message} | backtrace: #{e.backtrace}"
         { error: e.message, backtrace: e.backtrace }
